83b218cd268ccdbb29b0e73b346ce5057c7fd971
Oskaratam <oskaratamanenko2@gmail.com>
2026-03-14 23:22:38 +0100

    Add KworbScraper class

--- scripts/utils/etl_config.py
+++ scripts/utils/etl_config.py
@@ -6,6 +6,8 @@ YOUTUBE_API_PLAYLIST_SEARCH_PHRASES = ["world hits of 2026"]
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_PLAYLIST_ITEMS_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
 YOUTUBE_VIDEO_DETAILS_URL = "https://www.googleapis.com/youtube/v3/videos"
+KWORB_WORLDWIDE_CHART_URL = "https://kworb.net/ww/"
+KWORB_RADIO_CHART_URL = "https://kworb.net/radio/"
 
 category_to_table_name_map = {
     "youtube_videos": "bronze.youtube_videos",
